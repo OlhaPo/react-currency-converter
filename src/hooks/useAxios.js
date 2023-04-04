c1f19bd2524ccd0d4f92f952ff5fd6f412163dd4
Olya <ponomaryova.olya.y@gmail.com>
2023-04-04 14:35:37 +0200

    Add list of countries with currencies to options

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,27 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const UseAxios = () => {
-  return <div></div>;
+const useAxios = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoaded(true);
+        const response = await axios(url);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoaded(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return [data, error, loaded];
 };
 
-export default UseAxios;
+export default useAxios;
